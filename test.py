a6e8fa2cae870c243ea8c85466c09fcc1fd14ad8
Roman48RUS <realroman444@ya.ru>
2020-03-30 18:05:53 +0300

    Added get_time function, run test.py to see, how it works

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from requests import get
+from help_functions import get_time
 
-print(get('http://localhost:5000/api/jobs').json())
-print(get('http://localhost:5000/api/jobs/4').json())
-print(get('http://localhost:5000/api/jobs/404').json())
-print(get('http://localhost:5000/api/jobs/job_4').json())
+# print(get('http://localhost:5000/api/jobs').json())
+# print(get('http://localhost:5000/api/jobs/4').json())
+# print(get('http://localhost:5000/api/jobs/404').json())
+# print(get('http://localhost:5000/api/jobs/job_4').json())
+print(get_time("2019-03-30", "20:00"))
+print(get_time("2019-03-29", "20:00"))
+print(get_time("2019-03-28", "20:00"))

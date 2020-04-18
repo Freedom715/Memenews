1e59e2b6e73e4818fe32efcc960187eb9b737bc6
Svoboda21 <zotova_2003@bk.ru>
2020-04-18 19:17:33 +0300

    Пояснительная записка и презентация плюс попытка нейросети

--- analize.py
+++ analize.py
@@ -2,19 +2,21 @@ from fastai.imports import torch
 from fastai.vision import load_learner, defaults, open_image
 
 defaults.device = torch.device('cpu')
-learn = load_learner('pets')
+learn = load_learner('neuro')
 
 
 def analyze_image(filename):
     img = open_image(filename)
+    print(img)
     pred_class, pred_idx, outputs = learn.predict(img)
-    #return pred_class
-
+    # return pred_class
 
     pred = sorted(
         zip(learn.data.classes, map(float, outputs)),
         key=lambda p: p[1],
         reverse=True
     )
-    #return pred_class
-    return pred_class, pred[:2]
+    return pred_class
+
+
+print(analyze_image('E:\PyCharmProjects\Project_WEB\static\img\avatars\Alex_Pan.JPG'))
